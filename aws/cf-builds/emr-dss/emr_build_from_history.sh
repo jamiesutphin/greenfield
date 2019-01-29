c1360a8c8033f261b75181bced1cb6f6746fef6e
jamiesutphin <jamie.sutphin@gmail.com>
2019-01-29 10:46:32 -0500

    updated yum commands

--- aws/cf-builds/emr-dss/emr_build_from_history.sh
+++ aws/cf-builds/emr-dss/emr_build_from_history.sh
@@ -8,16 +8,16 @@
 # Dss license is found on an S3 bucket, configured in CF parameter
 # This Bootstrap script is in the same S3 location as the CF parameter
 
-set -x 
+set -x
 
-sudo yum update -yes
-sudo yum install jq -yes
+sudo yum update -y
+sudo yum install jq -y
 
 # only run on the master node
 IS_MASTER=$(curl http://169.254.169.254/latest/meta-data/security-groups | grep -i master)
 echo $IS_MASTER
 
-## [[ -z $IS_MASTER ]] && exit 0
+[[ -z $IS_MASTER ]] && exit 0
 
 WHOAMI=$(/usr/bin/whoami)
 
@@ -34,7 +34,7 @@ TEMPJSON=/tmp/out.json
 aws ec2 create-volume --size 10 --region us-east-1 --availability-zone $EMR_AWS_AZ --volume-type gp2 --tag-specifications 'ResourceType=volume,Tags=[{Key=purpose,Value=dss}]' > $TEMPJSON
 
 sleep 5
-EBS_VOL=$(jq ".VolumeId" $TEMPJSON | sed -r s/\"//g) 
+EBS_VOL=$(jq ".VolumeId" $TEMPJSON | sed -r s/\"//g)
 echo $EBS_VOL
 #wait for ebs volume
 sleep 35
@@ -42,7 +42,7 @@ sleep 35
 #wait for creation
 #EBS_VOL=
 #if [ -z $EBS_VOL ] ; then
-	EBS_VOL=$(jq ".VolumeId " $TEMPJSON | sed -r s/\"//g) 
+	EBS_VOL=$(jq ".VolumeId " $TEMPJSON | sed -r s/\"//g)
 	# EBS_VOL=$(aws ec2 describe-volumes | jq ".Volumes | .[] | select(.Tags | .[] | .Value == \"dss\" ) .VolumeId" | sed -r s/\"//g)
 #	sleep 15
 #fi
@@ -52,12 +52,9 @@ echo $INST
 
 sleep 5
 BLOCK_DEVICE=/dev/xvdf
-aws ec2 attach-volume --volume-id $EBS_VOL --instance-id $INST --device $BLOCK_DEVICE 
+aws ec2 attach-volume --volume-id $EBS_VOL --instance-id $INST --device $BLOCK_DEVICE
 
-#####
-exit 0 
-
-sleep 15
+sleep 30
 DATAIKU_BASE=/dataiku
 DSS_DATA_DIR=/dataiku/data
 sleep 5
@@ -66,8 +63,9 @@ sudo file -s $BLOCK_DEVICE
 sudo mkfs -t xfs $BLOCK_DEVICE
 sudo mkdir $DATAIKU_BASE
 sudo mount $BLOCK_DEVICE $DATAIKU_BASE
+sleep 10
 sudo chown -R ec2-user $DATAIKU_BASE
-mkdir $DSS_DATA_DIR
+mkdir -p $DSS_DATA_DIR
 
 sleep 5
 
@@ -82,8 +80,6 @@ DSS_TAR=dataiku-dss-$DSS_VERS_NUMBER.tar.gz
 wget -O $DATAIKU_BASE/$DSS_TAR "https://cdn.downloads.dataiku.com/public/dss/$DSS_VERS_NUMBER/$DATAIKU_VERSION.tar.gz"
 cd $DATAIKU_BASE
 tar xzf $DSS_TAR
-
-
 
 sudo -i $DATAIKU_BASE/$DATAIKU_VERSION/scripts/install/install-deps.sh -yes
 
